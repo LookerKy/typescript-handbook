f25378443f27640513590f359753faf486c91ae5
LookerKy <nagaraz07@gmail.com>
2020-08-13 18:53:28 +0900

    indexable type study

--- src/interface/indexable.ts
+++ src/interface/indexable.ts
@@ -6,3 +6,20 @@ let myArray: StringArray;
 myArray = ["Bob", "Fred"];
 
 let myStr: string = myArray[0];
+
+console.log(myStr);
+
+class Animal {
+  name: string;
+}
+
+class Dog extends Animal {
+  breed: string;
+}
+
+interface NotOkay {
+  [x: number]: Dog;
+  [x: string]: Dog;
+}
+
+let myAnimal: NotOkay;
